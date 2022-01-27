7d629ddfb85fdf9ddd547dd3ce371576ba311c5e
Hatim Alhazmi <78240511+hatimalih@users.noreply.github.com>
2022-01-27 10:47:28 -0500

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 """
 Python Core object Types
 """
@@ -11,22 +10,22 @@ def numbers_and_strings():
     This is to review numbers and strings and basic operations.
     """
     # Write the value 4 to the pwor of 5
-    x = 4**5
+    x = 4 ** 5
 
     # Assign a string "Stevens" to a variable y
     y = "Stevens"
 
     # Repeat variable y 7 times
-    z = y*7
+    z = y * 7
 
     # What is the length of z?
     length = len(z)
 
-    # Concatenate variable y with string " is Great"
+    # Concatenate variable y with string " Great"
     m = y + " is Great"
 
     # Replace "Great" with "Good" in variable m and assign it to a new variable n
-    n = m.replace("Great","Good")
+    n = m.replace("Great", "Good")
 
     return x, y, z, length, m, n
 
@@ -38,7 +37,7 @@ def lists():
     n = "Hoboken is awesome"
 
     # Split variable n on a delimiter space into a list of substrings
-    p = n.split(' , ')
+    p = n.split(" ")
 
     # Get all the items past the first of the third substring
     r = p[2][1:]
@@ -47,20 +46,18 @@ def lists():
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    A = [[1,4,5],
-         [6,10,11],
-         [12,17,38]]
+    A = [[1,4,5],[6,10,11],[12,17,38]]
 
     # Collect the items in the last column of matrix A using list comprehension
-    c = [col[2]for col in A]
+    c = [r[2] for r in A]
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    d = [A[i][i] for i in [0,1,2] if A[i][i]%2==0]
+    d = [A[i][i] for i in range(3) if A[i][i] % 2 == 0]
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-    o = [ord(s) for s in 'Hoboken']
+    o = [ord(i) for i in "Hoboken"]
 
     return p, r, c, d, o
 
@@ -73,28 +70,26 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 18
     #   color => "red"
-    f = {"fruit" :"apple", "quantity":18,"color":"red"}
+    f = {"fruit":"apple", "quantity":18, "color":"red"}
 
     # Get the item in dictionary f that the key "fruit" maps to
-    a = f['fruit']
+    a = f["fruit"]
     # Increase the quantity of f by 1
-    
-    f['quantity']+1
-    
+    f["quantity"] += 1
+
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-    amazing_grace = {'name':{'first_name':'Grace','last_name':'Hopper'},
-                    'jobs':['scientist',"engineer"],
-                    'age':85} 
+    amazing_grace = {"name":{"first_name":"Grace", "last_name":"Hopper"},
+                     "jobs":["scientist", "engineer"],
+                     "age":85}
 
     # Add "programmer" to the list of jobs Grace has
-    amazing_grace['jobs'].append('programmer')
-
+    amazing_grace["jobs"].append("programmer")
 
     # Get the third job Grace has that you recently added
-    p = amazing_grace['jobs'][-1]
+    p = amazing_grace["jobs"][2]
 
     # Get the sorted keys of amazing_grace in alphabetically ascending order
     k = list(amazing_grace.keys())
@@ -103,10 +98,6 @@ def dictionaries():
     return a, f, p, k
 
 
-
-
-
 numbers_and_strings()
 lists()
 dictionaries()
-
